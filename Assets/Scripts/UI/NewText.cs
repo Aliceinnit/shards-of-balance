6dd4b89a984c722bcefd27c6992efadb57ce3d62
ellalinl <ellalinl@chalmers.se>
2026-02-23 11:06:58 +0100

    added text tutorial

---
+++ Assets/Scripts/UI/NewText.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewText : MonoBehaviour
+{
+    public GameObject oldText;
+    public GameObject newText;
+    public float activationXPosition;
+    private Transform playerPosition;
+    private bool hasActivated = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerPosition = GameObject.Find("bruh").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasActivated && !oldText.activeSelf && playerPosition.position.x > activationXPosition)
+        {
+            hasActivated = true;
+            newText.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TutorialText.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TutorialText : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    //private Player playerMovements;
+    void Start()
+    {
+        //playerMovements = GameObject.Find("bruh").GetComponent<Player>();
+
+        if (textComponent == null)
+        {
+            Debug.LogError("Dialogue: textComponent is not assigned.");
+            enabled = false;
+            return;
+        }
+
+        if (lines == null || lines.Length == 0)
+        {
+            Debug.LogWarning("Dialogue: no lines assigned.");
+            enabled = false;
+            return;
+        }
+
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerMovements.enabled = false;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            //playerMovements.enabled = true;
+            //gameObject.SetActive(false);
+        }
+    }
+}
